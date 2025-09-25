9fb46e6ad792d7024bef4e4dad5b1787d558682b
mohith Doddi <mohithdoddi6@gmail.com>
2025-09-25 14:07:05 +0000

    please solve the error of the storageBucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5028942890-e7945",
   "appId": "1:1027557967324:web:e830928d6e5dd6156b3b19",
+  "storageBucket": "studio-5028942890-e7945.appspot.com",
   "apiKey": "AIzaSyCQaDIyXkip1_Y68NtaxEQ9Y7_7pEiz1Q0",
   "authDomain": "studio-5028942890-e7945.firebaseapp.com",
   "measurementId": "",
